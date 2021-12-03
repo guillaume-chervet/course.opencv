5d3d0f38696ea8fd142e3a9f2ceacb4c818fe8d7
guillaume chervet <guillaume.chervet@gmail.com>
2021-12-03 21:07:19 +0100

    update

--- 3-feature-matching/video.py
+++ 3-feature-matching/video.py
@@ -11,7 +11,7 @@ def apply_template(img_rgb, template):
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     w, h = template.shape[::-1]
     res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.6
+    threshold = 0.64
     loc = np.where(res >= threshold)
     for pt in zip(*loc[::-1]):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
